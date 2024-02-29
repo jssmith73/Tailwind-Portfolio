41e2204f9a399c080146c8290bdd3c372f226b42
jman1359 <josiahsmith1359@gmail.com>
2024-02-28 22:33:36 -0500

    Restructuring template and adding ME

---
+++ src/components/navigation.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            Josiah Smith
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            {/* About */}
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            {/* Technologies */}
+            <li>
+              <a href="#services" className="page-scroll">
+              Technologies
+              </a>
+            </li>
+            {/* Projects */}
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Projects
+              </a>
+            </li>
+            {/* Contact */}
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
